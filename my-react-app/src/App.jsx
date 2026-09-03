5e270bc510557c5f84ffa835fdbb33915eb22afc
yasmine88900 <ytag86081@gmail.com>
2026-09-03 14:55:05 +0100

    into

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Header from "./assets/Header"
+import Footer from "./assets/Footer";
+import Food from "./assets/Food";
+function App() {
+  return(
+    <>
+       <Header/>
+       <Food/>
+       <Footer/>
+    </>
+  );
+}
+
+
+
+export default App
