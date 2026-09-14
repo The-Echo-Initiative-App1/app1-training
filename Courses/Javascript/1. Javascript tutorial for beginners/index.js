8b605ba6a7f2bdba9d435c70c26109f00c216a54
cheataboo-byte <cheataboo@gmail.com>
2026-09-14 20:41:16 +0700

    Completed Variables, operators

---
+++ Courses/Javascript/1. Javascript tutorial for beginners/index.js
@@ -0,0 +1,10 @@
+//console.log(`Hello`);
+//console.log(`I like pizza`);
+
+//window.alert(`This is an alert`);
+//window.alert(`I like pizza, and there's nothing you can do about it`); 
+
+let students= 35;
+
+students--;
+
