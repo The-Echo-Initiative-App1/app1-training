8abb0212c32fca7c2caa102630e4d5263d422518
cheataboo-byte <cheataboo@gmail.com>
2026-09-15 19:49:35 +0700

    Completed Counter programs up until Strict Equality

--- Courses/Javascript/1. Javascript tutorial for beginners/index.js
+++ Courses/Javascript/1. Javascript tutorial for beginners/index.js
@@ -1,20 +1,69 @@
-let username;
-document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").ariaValueMax;
-    document.getElementById("myH1").textContent = Hello $("username")
+const decreasebtn = document.getElementById("decreasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const increasebtn = document.getElementById("increasebtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+increasebtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreasebtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+resetbtnbtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
 }
 
-let age = window.prompt("How old are you?");
-age+=1;
-age= Number(age);
-console.log(age, typeof age);
+Math.PI
+console.log(Math.PI);
+console.log(E);
+let x = 3.21;
+let z;
+const min = 50;
+const max = 100;
+let random = Math.floor(Math.random() * 6) + 1;
+console.log(randomNum);
 
-let pi = 3.14159
-let radius;
-let circumference;
+let age = 25;
+if(age >=18){
+    console.log("You are old enough to enter this site");
+}
+else{
+    console.log("You must be 18+ to enter this site");
+}
+let isStudent = false;
+if(isStudent){
+    console.log("You are a student!");
+}
+else{
+    console.log("You are NOT a student");
+}
+let age = 23;
+let message = age>= ? "You are an adult" : "You are a minor";
+console.log(message);
 
-raidius = window.prompt('Enter the radius of a circle');
-radius = Number(radius);
+let time = 16;
+let greeting = time < 12 ? "Good Morning!": "Good Afternoon!";
+console.log(greeting);
 
-circumference = 2 * pi * radius;
-console.log(circumference);
+let username = window.prompt("Enter your username");
+username = username.trim():
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+username = letter + extraChars;
+
+console.log(username);
+const temp = 20;
+if(temp > 0){
+    console.log("Thea weather is GREAT");
+}
+else if(temp <=30){
+    console.log("The weather is GOOD");
+}
+else(
+    console.log("The weather is BAD");
+)
