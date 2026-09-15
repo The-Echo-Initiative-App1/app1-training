3ff3f493e437c23b188700f850742a175621c285
cheataboo-byte <cheataboo@gmail.com>
2026-09-15 19:14:16 +0700

    Completed Accept user input, type conversion, contants.

--- Courses/Javascript/1. Javascript tutorial for beginners/index.js
+++ Courses/Javascript/1. Javascript tutorial for beginners/index.js
@@ -1,10 +1,20 @@
-//console.log(`Hello`);
-//console.log(`I like pizza`);
+let username;
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").ariaValueMax;
+    document.getElementById("myH1").textContent = Hello $("username")
+}
 
-//window.alert(`This is an alert`);
-//window.alert(`I like pizza, and there's nothing you can do about it`); 
+let age = window.prompt("How old are you?");
+age+=1;
+age= Number(age);
+console.log(age, typeof age);
 
-let students= 35;
+let pi = 3.14159
+let radius;
+let circumference;
 
-students--;
+raidius = window.prompt('Enter the radius of a circle');
+radius = Number(radius);
 
+circumference = 2 * pi * radius;
+console.log(circumference);
